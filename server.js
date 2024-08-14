4f2b8997f72144a2f23e1fa8d253fe5d83b38f07
Martin Krištof <martin.kristof@heureka.group>
2024-08-14 13:29:51 +0200

    feat: add reset

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // server.js
 const jsonServer = require('json-server')
+const reset = require('json-server-reset')
+const initJsonServerReset = require('json-server-reset/src/init-reset')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
+server.use(reset)
 server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
+const clear = ['posts']
+server.use(initJsonServerReset({ db: router.db, clear }))
+server.listen(process.env.PORT || 3020, () => {
   console.log('JSON Server is running')
 })
